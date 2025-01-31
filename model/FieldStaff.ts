b76b81d3b291cc201c41971c0b1a308d60d39d14
pavithrani <pererapavithrani02@gmail.com>
2025-01-31 19:39:00 +0530

    Develop field-staff backend

---
+++ model/FieldStaff.ts
@@ -0,0 +1,9 @@
+export class FieldStaff {
+    fieldId: string;
+    staffId: string;
+
+    constructor(fieldId: string, staffId: string) {
+        this.fieldId = fieldId;
+        this.staffId = staffId;
+    }
+}
